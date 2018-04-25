5dd7b4488cab5862b906938bf235af8f3a58e603
dogwata <35542785+dogwata@users.noreply.github.com>
2018-04-25 02:54:38 -0600

    Update script.js

--- myphotos/public/script.js
+++ myphotos/public/script.js
@@ -1,25 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
-    photos: [ { path: '/photos/mountain.jpg',
-		    attribution: 'Wendelin Jacober',
-		    original: 'https://www.flickr.com/photos/wendelinjacober/31763413875/in/photolist-QoPNjP-6hPUf-4C4d3J-5JB4ms-fNFC8t-5i24so-aVwo6F-AbUvu-7fVpVr-RUkNa7-bpThgu-kyyEf4-bvDE1b-9T21o8-6n9oAJ-5MQzoB-S3i8TJ-5zh6fK-JfhkBL-bv4kfQ-4sU4AW-P2cjhQ-5Q1hv9-8nTCN3-r3wwVY-jnE45z-aStYjP-7uPHkr-paCy38-5UptAb-3y7e8-djXWrH-78QKEV-3gPxBa-5V6g41-hcB1MR-7uPiTe-8C68ik-T4wj2r-5wNoFA-qrddA-6nvNR3-fo1MeZ-qZZ7xF-aK6CPe-6o32kc-arE5Tx-gPegwE-z1uzP-bnUZHA'
-		  },
-		  { path: '/photos/dog.jpg',
-		    attribution: 'Mario Antonio Pena Zapatería',
-		    original: 'https://www.flickr.com/photos/oneras/59865410/in/photolist-6hPUf-4C4d3J-5JB4ms-fNFC8t-5i24so-aVwo6F-AbUvu-7fVpVr-RUkNa7-bpThgu-kyyEf4-bvDE1b-9T21o8-6n9oAJ-5MQzoB-S3i8TJ-5zh6fK-JfhkBL-bv4kfQ-4sU4AW-P2cjhQ-5Q1hv9-8nTCN3-r3wwVY-jnE45z-aStYjP-7uPHkr-paCy38-5UptAb-3y7e8-djXWrH-78QKEV-3gPxBa-5V6g41-hcB1MR-7uPiTe-8C68ik-T4wj2r-5wNoFA-qrddA-6nvNR3-fo1MeZ-qZZ7xF-aK6CPe-6o32kc-arE5Tx-gPegwE-z1uzP-bnUZHA-jfb1c8'
-		  },
-		  { path: '/photos/child.jpg',
-		    attribution: 'elPadawan',
-		    original: 'https://www.flickr.com/photos/elpadawan/10967263754/in/photolist-hH96qo-aYtCHF-bCNe7e-djXX8V-oG7yr-7d8Wj-U9Yxd9-fGknQH-aYtCDt-agS2PN-dZpPRc-Tfv4AH-E2wsrd-fztFjY-7uTErN-AbQht-7uP7SF-VUQMga-7uTe1Y-jnHUzU-dSZ8bH-aYtCTP-qZSdpd-U6YSuk-rhiQSF-q2Nrv3-6giLQK-AbQho-5JgSLj-RUXW8j-9PjG41-7uPxrt-oQMizK-7uPD3R-8d1Fwa-eT2ts-hZHTnU-7wGJUC-9JYfD-jqL4hG-CR6N8t-izaUSA-78nw3x-7uTqKS-4w7rNk-4vic-kyyAQZ-JmnAPg-DGUFCj-HZTh65'
-		  },
-		  { path: '/photos/boats.jpg',
-		    attribution: 'gnuckx',
-		    original: 'https://www.flickr.com/photos/gnuckx/11640482934/in/photolist-iJCw2C-eiHzdn-dLGpP4-2Aa1m-5vDtx9-DGUFCj-21HtU7B-8Tq1T4-bW5Y1G-srMVoY-GoAfKf-5iZYA9-5Y5aXg-h3wDKz-793CNs-7uPurV-Uio2o4-5U7wXF-7uTZTw-Uio3uT-fDgGY3-Uio2U4-8HaDF9-6Qgwtt-6k65Wz-99dJ76-T1ZyfU-ZxrhRW-9mjhGW-7hitvz-nU8s8e-nbQN1D-9sTWM-63sL1Q-SMsBNb-T1Zz9h-dfT8Xx-7poUJQ-5XHaFA-5S4AuW-bG3EAn-eJ9F1i-58bs-6ZBdcb-5WEQ8R-9A6GJZ-7c22F1-7k7yW8-FVSQCC-7uTQpf'
-		  },
-		  { path: '/photos/taormina.jpg',
-		    attribution: 'gnuckx',
-		    original: 'https://www.flickr.com/photos/gnuckx/3667269012/in/photolist-6A4Hj5-ejGC9C-6zZxek-23fLveF-22b7TcQ-DAYFLt-9ELUwg-TLjbf2-3chrHG-GS4Emf-rDSMc8-ejASpg-ejGZEG-7hpKXx-5RCuXr-epCQg-bghrTB-ejGBUy-pMxxUf-aM8KSt-AX6Ywr-ejGAPw-5eCEbf-Z8oJAG-6A2gKg-6A55hw-6zZPXD-6A534J-6A6rkJ-7vVaCK-6A6pP9-pmRTHX-q4GU3E-pB2ifw-ejGUK1-5dwbZ4-6zZxwR-6zZyo2-G4hxWL-Ger4h-dPNp4Y-6zZLeD-hsq3wq-q2MyBS-pjCq5z-gBfvG-6A1S9g-TnQex1-p86x41-6A2kxP'
-		  } ]
+/* Couldn't get the following to work once I edited things to have descriptions show up in server records:
+    ip: "localhost"
+    photos: {}},
+    attribution: [],
+    original: [],
+  },
+  created: function() {
+    this.getPhotos();
+    this.getAttribution();
+    this.getOriginal();
+  },
+methods: {
+  getPhotos: function() {
+    axios.get("http://"+ this.ip + ":8080/api/photos").then(response => {
+      photo.path = photos.data;
+      return true;
+    }).catch(err => {
+    });
+},*/
+   photos: [ { path: '/photos/grass.jpg',
+  description: 'There are no snakes in that grass. I think.',
+  original: 'https://www.flickr.com/photos/aris_gionis/2486451369/in/photolist-4MHHnc-9QMSG1-jA76uk-gwWqzr-gwTJZ1-2YWRbK-dm2Wz8-2YWQXa-nn69RD-yLUNVe-nan35P-8T25jc-9gAGyt-9gFZ3h-k98KEL-HZi9Q9-a9nbDK-oVbgi3-cB22Yu-9iWop3-nQ6zGH-9Bi438-74C5kr-aPceC-dQeqAL-nNBMbS-21TWLWL-diuDYm-cWFriG-24aRn1G-EGWvTZ-5PhMUD-9jzUAj-57v82T-dS1f5p-Eofxc-SqudM-bd2oug-gwVVnw-Gt3gav-ptSZSy-4b81H1-ahXHgB-5KCSgd-d2a44m-5KDeRf-f3ukE1-dnqEnw-d2a2Ny-oe5Ucg'
+},
+{ path: '/photos/room.jpg',
+  description: 'If you keep adding layers of paint to opposite walls, how many layers are needed until the walls touch?',
+  original: 'https://www.flickr.com/photos/milestonemanagement/25515592797/in/photolist-ESJ5MK-25hHzGN-9Y8Kj3-ZUrj48-RND2eC-MaAib7-TSmiVG-248dM7o-RLxy4y-238EuD7-GyW76e-ZKeuZq-naDgAJ-22kjVRF-214EFZx-p5DpWf-238fkxm-WCuCNj-SdfQ3L-WoB1J4-aMWqeR-25YTLeZ-brd3vY-ZTj97t-HLcnXG-25g2HXm-ZSAszF-SrpQCY-YRKz6y-SajyaM-fq2cb5-ZJa5PC-S6H1Vw-EHKudz-D4HUEz-UquNJg-23USMwG-XwhDTC-VNaKxQ-Fxr2Ms-26arWwt-pvzxN6-BKMz8U-f7eW6G-GH86kP-cfrxdL-CyEx7F-U7ArTN-23dvsMb-TkwR8b'
+},
+{ path: '/photos/office.jpg',
+  description: 'I have only one criteria for our new office space. It needs a mailbox built right into the wall.',
+  original: 'https://www.flickr.com/photos/82134796@N03/39463418854/in/photolist-238fkxm-WCuCNj-SdfQ3L-WoB1J4-aMWqeR-25YTLeZ-brd3vY-ZTj97t-HLcnXG-25g2HXm-ZSAszF-SrpQCY-YRKz6y-SajyaM-fq2cb5-ZJa5PC-S6H1Vw-EHKudz-D4HUEz-UquNJg-23USMwG-XwhDTC-VNaKxQ-Fxr2Ms-26arWwt-pvzxN6-BKMz8U-f7eW6G-GH86kP-cfrxdL-CyEx7F-U7ArTN-23dvsMb-TkwR8b-22gFJ8x-pb4Wcq-ScHwMP-GPPTP4-21GxBwg-J5BJUf-brcR9N-245FnC1-EodbrP-GyW6hF-R4Skp4-rpktUz-ePNjVU-Fdtaxr-24QQ4Nz-21t2T9X'
+},
+{ path: '/photos/pool.jpg',
+  description: 'The kids said they wanted a house with a swimming pool in the backyard.',
+  original: 'https://www.flickr.com/photos/arg_flickr/41087831562/in/photolist-25AMSR1-ESJ5MK-25hHzGN-9Y8Kj3-ZUrj48-RND2eC-MaAib7-TSmiVG-248dM7o-RLxy4y-238EuD7-GyW76e-ZKeuZq-naDgAJ-22kjVRF-214EFZx-p5DpWf-238fkxm-WCuCNj-SdfQ3L-WoB1J4-aMWqeR-25YTLeZ-brd3vY-ZTj97t-HLcnXG-25g2HXm-ZSAszF-SrpQCY-YRKz6y-SajyaM-fq2cb5-ZJa5PC-S6H1Vw-EHKudz-D4HUEz-UquNJg-23USMwG-XwhDTC-VNaKxQ-Fxr2Ms-26arWwt-pvzxN6-BKMz8U-f7eW6G-GH86kP-cfrxdL-CyEx7F-U7ArTN-23dvsMb'
+},
+{ path: '/photos/dogs.jpg',
+  description: 'Tough luck kid. We have the balloon now.',
+  original: 'https://www.flickr.com/photos/pdelaignorancia/25414018257/in/photolist-EHKudz-D4HUEz-UquNJg-23USMwG-XwhDTC-VNaKxQ-Fxr2Ms-26arWwt-pvzxN6-BKMz8U-f7eW6G-GH86kP-cfrxdL-CyEx7F-U7ArTN-23dvsMb-TkwR8b-22gFJ8x-pb4Wcq-ScHwMP-GPPTP4-21GxBwg-J5BJUf-brcR9N-245FnC1-EodbrP-GyW6hF-R4Skp4-rpktUz-ePNjVU-Fdtaxr-24QQ4Nz-21t2T9X-Z8JFmA-RjyoY1-23xsxfu-E4EZEu-23yiThy-J9qAw1-24PVE4K-6oVRRR-WsrVzw-ErdXQ6-21N3BkR-UoG19q-22wBmM9-GrnnoB-HVPWUC-25m333Z-ZueSx4'
+} ]
+
   },
 });
+
