86204b7b68815362d0bd3919a7be0f9bc5ec16b6
dogwata <35542785+dogwata@users.noreply.github.com>
2018-04-25 02:56:55 -0600

    Update knexfile.js

--- myphotos/knexfile.js
+++ myphotos/knexfile.js
@@ -1,6 +1,50 @@
 // Update with your config settings.
 
-module.exports = {
+/*module.exports = {
+
+  development: {
+      client: 'mariasql',
+      connection: {
+        unixSocket : '/var/run/mysqld/mysqld.sock',
+        user     : 'wata-z',
+        db : 'Photos',
+        charset  : 'utf8'
+      }
+    },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};*/
 
   development: {
     client: 'sqlite3',
